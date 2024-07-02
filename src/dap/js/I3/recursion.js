0ed618f9333ba452e75526f61b271f1b27e3d53f
Diego Pinilla <dpinilla@sistemasenlinea.com.co>
2024-07-02 15:25:49 -0500

    feat: Create js

--- src/dap/js/I3/recursion.js
+++ src/dap/js/I3/recursion.js
@@ -1,9 +1,9 @@
-const isEven = (n) => {
-    if (n === 0) return true;
-    if (n === 1) return false;
-    if (n < 0) return isEven(-n);
+const isEven = (num) => {
+    if (num === 0) return true;
+    if (num === 1) return false;
+    if (num < 0) return isEven(-num);
 
-    return isEven(n - 2);
+    return isEven(num - 2);
 }
 
 console.log(isEven(50));
