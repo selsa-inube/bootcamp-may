d443f339c4ba736109cfbb7b30841dd535699f99
SelsaErojas <erojas@sistemasenlinea.com.co>
2024-07-02 09:27:26 -0500

    fix: ajustes ejercicios javascript a7

--- src/Er/js/a7/ejercicio-3.js
+++ src/Er/js/a7/ejercicio-3.js
@@ -1,4 +1,3 @@
-//primer intento
 class PGroup {
     #members;
     constructor(members) {
@@ -11,23 +10,29 @@ class PGroup {
         }
         return new PGroup(this.#members.concat([value]));
     }
-
+  
     delete(value){
         if (!this.has(value)){
             return this;
         }
         return new PGroup(this.#members.filter(m => m !== value));
     }
-
+  
     has(value){
         return this.#members.includes(value);
     }
-
+  
     static empty = new PGroup([]);
-}
-
-let a = PGroup.empty.add("a");
-
-let ab = a.add("b");
-
-let b = ab.delete("a");
+  }
+  
+  let a = PGroup.empty.add("a");
+  
+  let ab = a.add("b");
+  
+  let b = ab.delete("a");
+  
+  console.log(b.has("b"));
+  
+  console.log(a.has("b"));
+  
+  console.log(b.has("a"));
