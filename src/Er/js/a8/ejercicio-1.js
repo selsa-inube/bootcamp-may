e1f21442b66750f4a59080fbd677eea95c9cc142
SelsaErojas <erojas@sistemasenlinea.com.co>
2024-07-04 09:15:04 -0500

    feat: Creacion ejercicios javascript a8

---
+++ src/Er/js/a8/ejercicio-1.js
@@ -0,0 +1,24 @@
+class MultiplicatorUnitFailure extends Error {}
+
+function primitiveMultiply(a, b) {
+  if (Math.random() < 0.2) {
+    return a * b;
+  } else {
+    throw new MultiplicatorUnitFailure("Klunk");
+  }
+}
+
+function reliableMultiply(a, b) {
+  while (true) {
+    try {
+      return primitiveMultiply(a, b);
+    } catch (e) {
+      if (!(e instanceof MultiplicatorUnitFailure))
+        throw e;
+    }
+  }
+}
+
+console.log(reliableMultiply(7, 7));//49
+
+console.log(reliableMultiply(6, 6));//36
